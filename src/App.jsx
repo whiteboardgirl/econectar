38d097fa4a68895b508f1c1c83aa378ffbc303f7
whiteboardgirl <154104217+whiteboardgirl@users.noreply.github.com>
2025-02-28 19:20:29 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div>
-      <h1>Hello Econectar!</h1>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
