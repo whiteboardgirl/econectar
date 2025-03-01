b69f24d90a6495d27dbede1ab41b5d50eb0edcab
whiteboardgirl <154104217+whiteboardgirl@users.noreply.github.com>
2025-02-28 20:46:30 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['styled-components']
+    }
+  },
+  resolve: {
+    alias: {
+      'styled-components': 'styled-components/dist/styled-components.esm.js'
+    }
+  }
 })
